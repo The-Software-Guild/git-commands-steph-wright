592da129808134ceac36e53da2084a78190292b2
steph-wright <wright.steph.00@gmail.com>
2022-10-11 11:57:15 +0100

    first java project

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main (String[] args) {
+        System.out.println("Stephanie Wright");
+        System.out.println("3 White Horse Yard");
+        System.out.println("07531334857");
+        System.out.println("Milton Keynes");
+    }
+}
